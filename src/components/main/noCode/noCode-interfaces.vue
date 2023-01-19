4f5fff92d543283bf5f246a743f0e494e32a1d77
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-01-19 12:01:54 +0700

    switched to vue

---
+++ src/components/main/noCode/noCode-interfaces.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+interface btns {
+	btnPrev: {}
+}
+</script>
---
+++ src/components/screens/main/startScreen/startScreen.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import './interface.vue'
+import './style.scss'
+</script>
+
+<template>
+	<header class="header" id="header">
+		<div class="header__container">
+			<div class="header__body">
+				<div class="header__logo">
+					<span class="icon-style">project</span>
+				</div>
+				<div class="header__nav">
+					<ul>
+						<li>about</li>
+						<li>p</li>
+						<li>p</li>
+						<li>p</li>
+						<li>p</li>
+						<li>p</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
