e149447403ecee843509657b34fef484b88f66a7
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-05 20:52:54 +0700

    its not work

--- docs/assets/notFound-0be1eb67.js
+++
@@ -1,1 +0,0 @@
-import{e,l as o,c as t,o as n,b as s}from"./index-df4584c8.js";const _={class:"notFound"},a=s("h1",null,"NotFound",-1),c=[a],p=e({__name:"notFound",setup(u){return o.push({name:"NotFound"}),(d,r)=>(n(),t("div",_,c))}});export{p as default};
