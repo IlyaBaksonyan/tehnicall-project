5bb64f1928cdc5e6a54e2e2e96fd823b2c6cb98d
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-05 20:25:40 +0700

    finally fix 404

---
+++ docs/assets/notFound-2ca3f4c1.js
@@ -0,0 +1,1 @@
+import{e,l as o,c as t,o as n,b as s}from"./index-3600ce00.js";const _={class:"notFound"},a=s("h1",null,"NotFound",-1),c=[a],p=e({__name:"notFound",setup(u){return o.push("/"),(d,r)=>(n(),t("div",_,c))}});export{p as default};
