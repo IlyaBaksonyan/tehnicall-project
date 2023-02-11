0c36cc437cca5b8abe6cc883c453abc31f8acae6
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-11 14:08:50 +0700

    fixes

--- src/components/FirstPage/FirstPage.vue
+++ src/components/FirstPage/FirstPage.vue
@@ -21,18 +21,18 @@ export default {
 	},
 	methods: {
 		ScrollToDeveloper(e: any) {
-			const scrollBottom =
-				e.target.scrollHeight - e.target.scrollTop - e.target.offsetHeight
-			const calcutaedHeightCrutch =
-				(document.querySelector('.crutch') as HTMLElement).clientHeight / 2
-
-			const elem =
-				window.scrollY +
-				document.querySelector('#developerApproach')!.getBoundingClientRect()
-					.y -
-				this.headerHeight
-
 			if (!this.mainState == true) {
+				const scrollBottom =
+					e.target.scrollHeight - e.target.scrollTop - e.target.offsetHeight
+				const calcutaedHeightCrutch =
+					(document.querySelector('.crutch') as HTMLElement).clientHeight / 2
+
+				const elem =
+					window.scrollY +
+					document.querySelector('#developerApproach')!.getBoundingClientRect()
+						.y -
+					this.headerHeight
+
 				if (scrollBottom <= calcutaedHeightCrutch) {
 					window.scrollTo({ top: elem })
 					document.body.style.overflow = 'auto'
@@ -44,18 +44,17 @@ export default {
 		},
 
 		ScrollToMain() {
-			const mainHeight = (
-				document.querySelector('.main') as HTMLElement
-			).getBoundingClientRect().height
-			const scrollTop = window.scrollY
-			const elem =
-				window.scrollY +
-				document.querySelector('.main')!.getBoundingClientRect().top -
-				this.headerHeight
-
 			if (!this.developerState == true) {
+				const mainHeight = (
+					document.querySelector('.main') as HTMLElement
+				).getBoundingClientRect().height
+				const scrollTop = window.scrollY
+				const elem =
+					window.scrollY +
+					document.querySelector('.main')!.getBoundingClientRect().top -
+					this.headerHeight
 				if (scrollTop <= mainHeight) {
-					window.scrollTo({ top: elem, behavior: 'smooth' })
+					window.scrollTo({ top: elem })
 
 					this.developerState = true
 					setTimeout(() => (this.developerState = false), 1000)
@@ -65,15 +64,18 @@ export default {
 	},
 
 	mounted() {
-		window.onscroll = () => {
-			setTimeout(() => this.ScrollToMain(), 1000)
+		const a = () => {
 			if (window.scrollY == 0) {
 				document.body.style.overflow = 'hidden'
 			}
 		}
-		if (window.scrollY == 0) {
-			document.body.style.overflow = 'hidden'
-		}
+		setTimeout(a, 500)
+		document.addEventListener('scroll', () => {
+			this.ScrollToMain()
+			if (window.scrollY == 0) {
+				document.body.style.overflow = 'hidden'
+			}
+		})
 	},
 	unmounted() {
 		document.body.removeAttribute('style')
@@ -105,28 +107,3 @@ export default {
 	min-height: 10vh;
 }
 </style>
-
-<!-- <script>
-// Get the current scroll position
-let currentScrollPosition = window.scrollY
-
-// Set the scroll position to the desired position
-let desiredScrollPosition = 100
-
-// Get the animation frame ID
-let animationFrameId = window.requestAnimationFrame(function () {
-	// Calculate the scroll amount
-	let scrollAmount = (desiredScrollPosition - currentScrollPosition) / 10
-
-	// Scroll the window
-	window.scrollTo(0, currentScrollPosition + scrollAmount)
-
-	// Update the current scroll position
-	currentScrollPosition = window.scrollY
-
-	// If the desired scroll position has not been reached, call the requestAnimationFrame() function again
-	if (currentScrollPosition < desiredScrollPosition) {
-		animationFrameId = window.requestAnimationFrame(arguments.callee)
-	}
-})
-</script> -->
