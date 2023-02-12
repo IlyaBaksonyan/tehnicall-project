4ccc3c7100fddd09f82b5c34202d83676df488d7
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-12 23:49:42 +0700

    please work

--- docs/_nuxt/app.config.8ba0fc63.js
+++
@@ -1,1 +0,0 @@
-import{k as u,K as s}from"./entry.8a4eb73f.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
---
+++ docs/assets/app.config.8572650c.js
@@ -0,0 +1,1 @@
+import{k as u,K as s}from"./entry.c5e116d9.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
