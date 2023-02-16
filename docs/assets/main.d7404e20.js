fceedfd9de09b0356be871179292a19db1241346
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-16 11:10:09 +0700

    update adaptive NoCode

--- docs/assets/main.d7404e20.js
+++
@@ -1,1 +0,0 @@
-import{a as e,o as c,b as r,c as _}from"./entry.524da5a3.js";const a={};function s(t,o,n,i,l,m){return c(),r("h1",null,"Общее")}const u=e(a,[["render",s]]),p={};function f(t,o){const n=u;return c(),_(n)}const $=e(p,[["render",f]]);export{$ as default};
