9a3d9ca88a7ffb6138e6c9535cd8e09fc203a5d4
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-19 19:57:12 +0700

    improve text, little update freelanerPage

--- docs/assets/app.config.29c1e5be.js
+++
@@ -1,1 +0,0 @@
-import{k as u,L as s}from"./entry.f8d12ad1.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
---
+++ docs/assets/app.config.9ad28b66.js
@@ -0,0 +1,1 @@
+import{k as u,L as s}from"./entry.bbd9ba64.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
