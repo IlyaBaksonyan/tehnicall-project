2c24099a5763dcf4a05365e6fea3db118411a8b7
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-23 20:57:43 +0700

    made cms definition section on freelancerPage

---
+++ docs/assets/protect.2d36f8de.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./protect.vue.927ae7fd.js";import"./entry.1fc1866e.js";export{m as default};
---
+++ docs/assets/settings.8cdc445b.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./settings.vue.3b174fea.js";import"./entry.1fc1866e.js";export{m as default};
