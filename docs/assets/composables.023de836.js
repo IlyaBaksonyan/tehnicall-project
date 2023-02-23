1b7dbdbe8e907d0fb2c7c48da788d645452afc15
baksonyan4ik <ilya.zateev.96@gmail.com>
2023-02-23 18:13:56 +0700

    add icons in menu FreelancerPage

---
+++ docs/assets/composables.023de836.js
@@ -0,0 +1,1 @@
+import{B as r}from"./entry.e1b4ab65.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
--- docs/assets/composables.c8a511b4.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./entry.9649509c.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
